ef1dc8cd95a6d6d0314703ec76fa85ec7c085ccd
Tarun Mandhan <mandhantarun@gmail.com>
2023-01-10 04:15:25 +0530

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Error from "./Error";
 
 const App = () => {
+  const Name = () => {
+    return <h1> this is Name page.</h1>;
+  };
   return (
     <>
-      <h1>Hello Tarun</h1>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact/Name" element={<Name />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 };
