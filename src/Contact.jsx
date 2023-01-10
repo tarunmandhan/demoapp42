a091811dd74f4cb43046aa132c1fb9d5502e0b21
Tarun Mandhan <mandhantarun@gmail.com>
2023-01-11 01:36:02 +0530

    useNavigate

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
+  const abc = useNavigate();
+
+  const goToHome = () => {
+    abc("/");
+  };
   return (
     <>
       <h1>Contact Pawan</h1>
+      <button onClick={goToHome}> go to Home page </button>
     </>
   );
 };
