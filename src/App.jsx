c94ef9098d0d698a17126d67cb1ddc74f4dd2063
Tarun Mandhan <mandhantarun@gmail.com>
2023-01-10 23:50:26 +0530

    add MainHeader

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import Contact from "./Contact";
 import Error from "./Error";
 import Home from "./Home";
 import "./App.css";
+import MainHeader from "./MainHeader";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}>
+          <Route path="/" element={<MainHeader />}>
+            <Route index element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<Error />} />
