4182863714e17e15b7de4c796d6ad9116e151c86
Tarun Mandhan <mandhantarun@gmail.com>
2023-01-10 21:04:05 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,15 @@ import Home from "./Home";
 import "./App.css";
 
 const App = () => {
-  const Name = () => {
-    return <h1> this is Name page.</h1>;
-  };
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Error />} />
-          <Route />
+          <Route path="/" element={<Home />}>
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Error />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
