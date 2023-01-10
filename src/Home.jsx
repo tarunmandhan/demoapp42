5ac217ccb45ff9d28babaa650e8c0cb346ae0d75
Tarun Mandhan <mandhantarun@gmail.com>
2023-01-10 23:49:39 +0530

    remove outlet

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
-import Header from "./Component/Header";
 
 const Home = () => {
   return (
     <>
-      <Header />
-      <Outlet />
-      {/* share Layout to others components needs Outlet */}
+      <h1>hello Home page </h1>
     </>
   );
 };
