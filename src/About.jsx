5fec5e06e11a95635cefe16bf2e0aca50ffe56d2
Tarun Mandhan <mandhantarun@gmail.com>
2023-01-10 21:54:46 +0530

    remove header component

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from "./Component/Header";
 
 const About = () => {
   return (
     <>
-      <Header />
       <h1>About tarun </h1>
     </>
   );
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from "./Component/Header";
 
 const Contact = () => {
   return (
     <>
-      <Header />
       <h1>Contact Pawan</h1>
     </>
   );
