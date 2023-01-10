db1e810cbdc5bf3da52bba18219d0d9d1ea6832b
Tarun Mandhan <mandhantarun@gmail.com>
2023-01-10 18:01:49 +0530

    logo & navbar

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <header>
+        <a href="#">Logo</a>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Home;
